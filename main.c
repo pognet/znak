90a0505496c23121fc9b23db0c2f0e3c127c731a
pognet <pogo@interbit.pl>
2018-10-12 10:59:30 +0200

    znak

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    char znak;
+    int wartosc_calkowita;
+    double wartosc_wymierna;
+
+    scanf("%c%i%lf",
+          &znak,
+          &wartosc_calkowita,
+          &wartosc_wymierna);
+
+    printf(
+            "znak = %c,"
+            "\n liczba całkowita = %i,"
+            "\n liczba wymierna = %lf",
+            znak,
+            wartosc_calkowita,
+            wartosc_wymierna
+            );
+
+    return 0;
+}
