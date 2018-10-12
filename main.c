6855fd1455952c27ad1e7d17883a575018bc6e74
pognet <pogo@interbit.pl>
2018-10-12 14:00:49 +0200

    ciąg znaków

--- main.c
+++ main.c
@@ -4,19 +4,27 @@ int main() {
     char znak;
     int wartosc_calkowita;
     double wartosc_wymierna;
+    char ciag_znakow[256];
 
-    scanf("%c%i%lf",
-          &znak,
-          &wartosc_calkowita,
-          &wartosc_wymierna);
+    printf("Podaj znak: ");
+    scanf("%c", &znak);
+    printf("Podaj ciąg znaków: "),
+    scanf("%s", &ciag_znakow);
+    // scanf("%s", &ciag_znakow[0]; // alternatywnie
+    printf("Podaj liczbę całkowitą: "), scanf("%i", & wartosc_calkowita);
+    printf("Podaj liczbę wymierną: "), scanf("%lf", &wartosc_wymierna);
 
+
+    printf("\n");
     printf(
-            "znak = %c,"
-            "\n liczba całkowita = %i,"
-            "\n liczba wymierna = %lf",
+            "znak = %c"
+            "\nciąg znaków = %s"
+            "\n liczba całkowita = %i"
+            "\n liczba wymierna = %lf"
             znak,
             wartosc_calkowita,
             wartosc_wymierna
+            ciag_znakowy
             );
 
     return 0;
